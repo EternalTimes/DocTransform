243d51707e501b8fdec980a2f220cdc20ba58d68
Eternal Times <Tirrammissu@outlook.com>
2025-05-26 16:29:34 +0800

    feat(Converters): Add StringListToStringConverter for UI display

---
+++ Converters/StringListToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DocTransform.Converters; // 确保命名空间正确
+
+public class StringListToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is IEnumerable<string> stringList && stringList.Any())
+        {
+            return string.Join(", ", stringList);
+        }
+        return "无";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
